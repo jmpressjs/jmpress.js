cd03fc122668b9ae8a6fa3c4e61c67f5989d887a
Kyle Robinson Young <kyle@dontkry.com>
2012-02-07 09:20:03 -0800

    Restructure project into a more standard structure

--- dist/jmpress.all.js
+++ dist/jmpress.all.js
@@ -2110,70 +2110,19 @@
 }(jQuery, document, window));
 (function( $, document, window, undefined ) {
 	'use strict';
-	$.jmpress("defaults").duration = {
-		defaultValue: -1
-		,defaultAction: "next"
-		,barSelector: undefined
-		,barProperty: "width"
-		,barPropertyStart: "0"
-		,barPropertyEnd: "100%"
-	};
-	$.jmpress("initStep", function( step, eventData ) {
-		eventData.stepData.duration = eventData.data.duration;
-		eventData.stepData.durationAction = eventData.data.durationAction;
-	});
-	$.jmpress("setInactive", function( step, eventData ) {
-		var dur = eventData.stepData.duration || eventData.settings.duration.defaultValue;
-		if( dur && dur > 0 ) {
-			if( eventData.settings.duration.barSelector ) {
-				var css = {
-					transitionProperty: eventData.settings.duration.barProperty
-					,transitionDuration: '0'
-					,transitionDelay: '0'
-					,transitionTimingFunction: 'linear'
-				};
-				css[eventData.settings.duration.barProperty] = eventData.settings.duration.barPropertyStart;
-				var bars = $(eventData.settings.duration.barSelector);
-				$.jmpress("css", bars, css);
-				bars.each(function(idx, element) {
-					var next = $(element).next();
-					var parent = $(element).parent();
-					$(element).detach();
-					if(next.length) {
-						next.insertBefore(element);
-					} else {
-						parent.append(element);
-					}
-				});
+	$.jmpress("register", "toggle", function( key, config, initial ) {
+		var jmpress = this;
+		$(document).bind("keydown", function( event ) {
+			if ( event.keyCode === key ) {
+				if ($(jmpress).jmpress("initialized")) {
+					$(jmpress).jmpress("deinit");
+				} else {
+					$(jmpress).jmpress(config);
+				}
 			}
-			if(eventData.current.durationTimeout) {
-				clearTimeout(eventData.current.durationTimeout);
-				eventData.current.durationTimeout = undefined;
-			}
-		}
-	});
-	$.jmpress("setActive", function( step, eventData ) {
-		var dur = eventData.stepData.duration || eventData.settings.duration.defaultValue;
-		if( dur && dur > 0 ) {
-			if( eventData.settings.duration.barSelector ) {
-				var css = {
-					transitionProperty: eventData.settings.duration.barProperty
-					,transitionDuration: (dur-eventData.settings.transitionDuration*2/3-100)+"ms"
-					,transitionDelay: (eventData.settings.transitionDuration*2/3)+'ms'
-					,transitionTimingFunction: 'linear'
-				};
-				css[eventData.settings.duration.barProperty] = eventData.settings.duration.barPropertyEnd;
-				$.jmpress("css", $(eventData.settings.duration.barSelector), css);
-			}
-			var jmpress = this;
-			if(eventData.current.durationTimeout) {
-				clearTimeout(eventData.current.durationTimeout);
-				eventData.current.durationTimeout = undefined;
-			}
-			eventData.current.durationTimeout = setTimeout(function() {
-				var action = eventData.stepData.durationAction || eventData.settings.duration.defaultAction;
-				$(jmpress).jmpress(action);
-			}, dur);
+		});
+		if ( initial ) {
+			$(jmpress).jmpress(config);
 		}
 	});
 }(jQuery, document, window));
@@ -2244,19 +2193,70 @@
 
 (function( $, document, window, undefined ) {
 	'use strict';
-	$.jmpress("register", "toggle", function( key, config, initial ) {
-		var jmpress = this;
-		$(document).bind("keydown", function( event ) {
-			if ( event.keyCode === key ) {
-				if ($(jmpress).jmpress("initialized")) {
-					$(jmpress).jmpress("deinit");
-				} else {
-					$(jmpress).jmpress(config);
-				}
+	$.jmpress("defaults").duration = {
+		defaultValue: -1
+		,defaultAction: "next"
+		,barSelector: undefined
+		,barProperty: "width"
+		,barPropertyStart: "0"
+		,barPropertyEnd: "100%"
+	};
+	$.jmpress("initStep", function( step, eventData ) {
+		eventData.stepData.duration = eventData.data.duration;
+		eventData.stepData.durationAction = eventData.data.durationAction;
+	});
+	$.jmpress("setInactive", function( step, eventData ) {
+		var dur = eventData.stepData.duration || eventData.settings.duration.defaultValue;
+		if( dur && dur > 0 ) {
+			if( eventData.settings.duration.barSelector ) {
+				var css = {
+					transitionProperty: eventData.settings.duration.barProperty
+					,transitionDuration: '0'
+					,transitionDelay: '0'
+					,transitionTimingFunction: 'linear'
+				};
+				css[eventData.settings.duration.barProperty] = eventData.settings.duration.barPropertyStart;
+				var bars = $(eventData.settings.duration.barSelector);
+				$.jmpress("css", bars, css);
+				bars.each(function(idx, element) {
+					var next = $(element).next();
+					var parent = $(element).parent();
+					$(element).detach();
+					if(next.length) {
+						next.insertBefore(element);
+					} else {
+						parent.append(element);
+					}
+				});
 			}
-		});
-		if ( initial ) {
-			$(jmpress).jmpress(config);
+			if(eventData.current.durationTimeout) {
+				clearTimeout(eventData.current.durationTimeout);
+				eventData.current.durationTimeout = undefined;
+			}
+		}
+	});
+	$.jmpress("setActive", function( step, eventData ) {
+		var dur = eventData.stepData.duration || eventData.settings.duration.defaultValue;
+		if( dur && dur > 0 ) {
+			if( eventData.settings.duration.barSelector ) {
+				var css = {
+					transitionProperty: eventData.settings.duration.barProperty
+					,transitionDuration: (dur-eventData.settings.transitionDuration*2/3-100)+"ms"
+					,transitionDelay: (eventData.settings.transitionDuration*2/3)+'ms'
+					,transitionTimingFunction: 'linear'
+				};
+				css[eventData.settings.duration.barProperty] = eventData.settings.duration.barPropertyEnd;
+				$.jmpress("css", $(eventData.settings.duration.barSelector), css);
+			}
+			var jmpress = this;
+			if(eventData.current.durationTimeout) {
+				clearTimeout(eventData.current.durationTimeout);
+				eventData.current.durationTimeout = undefined;
+			}
+			eventData.current.durationTimeout = setTimeout(function() {
+				var action = eventData.stepData.durationAction || eventData.settings.duration.defaultAction;
+				$(jmpress).jmpress(action);
+			}, dur);
 		}
 	});
 }(jQuery, document, window));
