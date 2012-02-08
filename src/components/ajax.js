ea20d7a3dcbdf4d8600f5e41b26d18cc621fb7bb
Kyle Robinson Young <kyle@dontkry.com>
2012-02-07 22:57:31 -0800

    Avoid error as eventData.stepData is not always set before loadStep

--- src/components/ajax.js
+++ src/components/ajax.js
@@ -11,7 +11,7 @@
 		eventData.stepData.src = $(step).attr('href') || eventData.data.src || false;
 	});
 	$.jmpress('loadStep', function( step, eventData ) {
-		var href = eventData.stepData.src;
+		var href = eventData.stepData ? eventData.stepData.src || false : false;
 		if ( href ) {
 			$(step).load(href, function(response, status, xhr) {
 				$(eventData.jmpress).jmpress('fire', 'afterStepLoaded', step, $.extend({}, eventData, {
