3c5c69343746d91acf9c61b2e27137bb337c0fee
Tobias Koppers <tobias.koppers@googlemail.com>
2012-02-25 02:35:20 +0100

    #26 iPad support, swipe support

--- src/components/mobile.js
+++ src/components/mobile.js
@@ -18,22 +18,27 @@
 			current = eventData.current,
 			jmpress = eventData.jmpress;
 		current.mobileNamespace = ".jmpress-"+randomString();
+		var data, start = [0,0];
 		$(settings.fullscreen ? document : jmpress)
 			.bind("touchstart"+current.mobileNamespace, function( event ) {
 
-			var data = event.orginalEvent.touches[0];
-			var start = [ data.pageX, data.pageY ];
-			
-			$(this).one("touchend", function( event ) {
-				var data = event.orginalEvent.touches[0],
-					end = [ data.pageX, data.pageY ],
-					diff = [ end[0]-start[0], end[1]-start[1] ];
+			data = event.originalEvent.touches[0];
+			start = [ data.pageX, data.pageY ];
 
-				if(Math.max(Math.abs(diff[0]), Math.abs(diff[1])) > 50) {
-					diff = Math.abs(diff[0]) > Math.abs(diff[1]) ? diff[0] : diff[1];
-					$(eventData.jmpress).jmpress(diff < 0 ? "prev" : "next");
-				}
-			});
+		}).bind("touchmove"+current.mobileNamespace, function( event ) {
+			data = event.originalEvent.touches[0];
+			event.preventDefault();
+			return false;
+		}).bind("touchend"+current.mobileNamespace, function( event ) {
+			var end = [ data.pageX, data.pageY ],
+				diff = [ end[0]-start[0], end[1]-start[1] ];
+
+			if(Math.max(Math.abs(diff[0]), Math.abs(diff[1])) > 50) {
+				diff = Math.abs(diff[0]) > Math.abs(diff[1]) ? diff[0] : diff[1];
+				$(jmpress).jmpress(diff > 0 ? "prev" : "next");
+				event.preventDefault();
+				return false;
+			}
 		});
 	});
 	$jmpress('afterDeinit', function( nil, eventData ) {
