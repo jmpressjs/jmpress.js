3605801335195e6da687b977d2d15711ab073272
Tobias Koppers <tobias.koppers@googlemail.com>
2012-05-07 20:54:51 +0200

    do not handle line breaks

--- docs/lib/docs/yamyam.loader.js
+++ docs/lib/docs/yamyam.loader.js
@@ -2,7 +2,15 @@ var YamYam = require("YamYam");
 
 module.exports = function(source) {
 	var cb = this.callback;
-	YamYam.parse(source, function(err, html) {
+	YamYam.parse(source, {
+		format: {
+			line: {
+				tag: "",
+				_prepend: " ",
+				_prependStart: ""
+			}
+		}
+	}, function(err, html) {
 		cb(err, html && ("module.exports =\n\t" + JSON.stringify(html) + ";"));
 	});
 }
