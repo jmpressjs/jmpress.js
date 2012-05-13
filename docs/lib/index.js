34c7fa690f8e35036e54e0cfbfba9ea6b603f886
Tobias Koppers <tobias.koppers@googlemail.com>
2012-05-13 12:58:49 +0200

    better tracking

--- docs/lib/index.js
+++ docs/lib/index.js
@@ -8,6 +8,7 @@ $(function() {
 		$(window).scrollTop(0);
 		var hash = location.hash;
 		hash = hash.replace(/^#/, "").split("-");
+		window._gaq.push(['_trackPageview', "/jmpress.js/docs/" + hash.join("/")]);
 		switch(hash.shift()) {
 		case "builder":
 			require("bundle!./builder")(function(page) {
@@ -28,3 +29,15 @@ $(function() {
 	});
 	$(window).hashchange();
 });
+//
+// IGNORE BELOW THIS LINE
+//
+var _gaq = window._gaq = window._gaq || [];
+_gaq.push(['_setAccount', 'UA-28251006-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
