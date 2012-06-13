1bd8c63fb46bb79902255737c3352c66b46d165a
Kyle Robinson Young <kyle@dontkry.com>
2012-06-13 14:53:49 -0700

    doc: clickSelects property on mouse

---
+++ docs/asserts/21.71322b29a1fd296091eae470d0f3ce8a.js
@@ -0,0 +1,1 @@
+webpackJsonp(21,{52:function(a,b,c){a.exports='<h1>Mouse</h1><p>This component handles clicking to activate steps.</p><h3><code>property</code> clickSelects : <code>true</code></h3><p>By default clicking to active a step is enabled. To disable:</p><pre class="language-javascript">$(&#39;#jmpress&#39;).jmpress({<br/>	clickSelects: false<br/>});</pre>'}})
--- src/docs/mouse.md
+++ src/docs/mouse.md
@@ -1,3 +1,13 @@
 # Mouse
 
-This component handles click on other steps.
+This component handles clicking to activate steps.
+
+### `property` clickSelects : `true`
+
+By default clicking to active a step is enabled. To disable:
+
+``` javascript
+$('#jmpress').jmpress({
+	clickSelects: false
+});
+```
