df261af5337ad109bae9ab49b4c1b476be76d3a2
Tobias Koppers <tobias.koppers@googlemail.com>
2012-07-24 08:37:31 +0300

    apply changes to duration correctly

--- src/plugins/duration.js
+++ src/plugins/duration.js
@@ -23,7 +23,7 @@
 			durationSettings = settings.duration,
 			current = eventData.current;
 		var dur = eventData.stepData.duration || durationSettings.defaultValue;
-		if( dur && dur > 0 ) {
+		if( current.durationTimeout ) {
 			if( durationSettings.barSelector ) {
 				var css = {
 					transitionProperty: durationSettings.barProperty
@@ -45,10 +45,8 @@
 					}
 				});
 			}
-			if(current.durationTimeout) {
-				clearTimeout(current.durationTimeout);
-				current.durationTimeout = undefined;
-			}
+			clearTimeout(current.durationTimeout);
+			delete current.durationTimeout;
 		}
 	});
 	$.jmpress("setActive", function( step, eventData ) {
