bc81cb61ad84d0e58b589f6fa4430d0a5ef82da0
Erich Seifert <dev@erichseifert.de>
2014-09-11 19:29:43 +0200

    Reverted uppercase changes to prefix names and returning lowercase name

--- src/components/core.js
+++ src/components/core.js
@@ -14,7 +14,7 @@
 	 */
 	var pfx = (function () {
 		var style = document.createElement('dummy').style,
-			prefixes = ['webkit', 'moz', 'o', 'ms', 'khtml', ''],
+			prefixes = ['Webkit', 'Moz', 'O', 'ms', 'Khtml', ''],
 			memory = {};
 		return function ( prop ) {
 			if ( typeof memory[ prop ] === "undefined" ) {
@@ -42,7 +42,7 @@
 		var index = 1 + name.substr(1).search(/[A-Z]/);
 		var prefix = name.substr(0, index).toLowerCase();
 		if(prefix === '') {
-			return name;
+			return name.toLowerCase();
 		}
 		var postfix = name.substr(index).toLowerCase();
 		return "-" + prefix + "-" + postfix;
